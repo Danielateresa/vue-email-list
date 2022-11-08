f3762ccfa39962733fab81deb76cd75cc2812f99
Danielateresa <dany.t_87@hotmail.it>
2022-11-08 14:12:23 +0100

    Printed emails in the list

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,26 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
-alert('ciao')
+
 const { createApp } = Vue;
 
 createApp({
     data() {
         return {
+            emailList: []
+        }
+    },
+    mounted() {
+        //chiamata ajax all'interno di un ciclo for
+        for (let index = 0; index < 10; index++) {
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => {
+                    //console.log(response.data.response);
+
+                    this.emailList.push(response.data.response)
+                });
+            //console.log(this.emailList);
 
         }
     }
